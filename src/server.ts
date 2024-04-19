c0ca37e952eae457d6f94022dacbc9e1a0cbbd8d
Aislan Santos <aislan.santos@gmail.com>
2024-04-19 15:20:25 -0300

    chore: add and setup server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,28 @@
 import * as express from "express";
 import { Request, Response } from "express";
-import Person from "@/index";
+import * as path from "path";
+import * as dotenv from "dotenv";
+import * as cors from "cors";
+import routes from "@/routes/routes";
 
-const app = express();
-const person = new Person();
+dotenv.config();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(person.sayMyName());
+const server = express();
+
+server.use(cors());
+
+server.use(express.json());
+
+server.use(express.static(path.join(__dirname, "../public")));
+server.use(express.urlencoded({ extended: true }));
+
+server.use("/", routes);
+
+server.use((req: Request, res: Response) => {
+  res.status(404);
+  res.json({ error: "Endpoint não encontrado." });
 });
 
-app.listen(3000, () => {
+server.listen(process.env.PORT, () => {
   console.log("Rodando na porta 3000");
 });
