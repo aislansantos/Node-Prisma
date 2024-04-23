03fad8415817938bb3cc0b09b54e9ec5a9fcb374
Aislan Santos <aislan.santos@gmail.com>
2024-04-22 21:58:25 -0300

    feat: setup schema prisma, config prisma.ts create new rote test and files migration prisma

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,5 +1,10 @@
+import { prisma } from "@/libs/prisma";
 import { Request, Response } from "express";
 
 export const ping = (req: Request, res: Response) => {
   return res.send("teste");
 };
+
+export const teste = (req: Request, res: Response) => {
+  prisma.user
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,5 +4,6 @@ import * as ApiControler from "@/controllers/api.controller";
 const router = Router();
 
 router.get("/", ApiControler.ping);
+router.get("/teste", ApiControler.teste);
 
 export default router;
